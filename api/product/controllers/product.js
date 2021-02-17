3db2a383e9e9e51e369f96adaaa7f10b9275b89b
Alexis Guzman <alexis12989x@gmail.com>
2021-02-17 17:47:21 -0600

    Create config for deploy in heroku and custom api

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,15 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // find: (ctx) => {
+  //     return strapi.query('poduct').find(ctx.query, ["items"])
+  // },
+  findOne: (ctx) => {
+    return strapi.query("product").findOne(ctx.query, ["info", "items"]);
+  },
+};
